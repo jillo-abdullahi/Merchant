8eb035199d403a18fe43c2e591680a4b284e75ed
samauser <samauser@SDC-2P081.samadc.corp>
2017-04-06 18:40:08 +0300

    Handling orders

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -5,4 +5,14 @@ module ProductsHelper
     #Using Rail's built-in helper, number_to_currency
     number_to_currency price
   end
+  def print_stock(stock)
+    #Use content_tag to inject html
+  if stock > 0
+    content_tag(:span, "In Stock #{stock}", class: "in_stock")
+
+  else
+    content_tag(:span, "Out of Stock", class: "Out of Stock")
+  end
+end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
 
   #Adding a validation for user price entry when creating a new form
   validates_numericality_of :price
+  validates_numericality_of :stock, :only_integer => true, :greater_than_or_equal_to => 0
 end
