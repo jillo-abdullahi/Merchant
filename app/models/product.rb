974078f012f538b1c06818ef5d7efd6cefa42654
samauser <samauser@SDC-2P081.samadc.corp>
2017-04-06 19:44:28 +0300

    Designing orders almost done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
 
+  has_many :order_items
   #Adding a validation for user price entry when creating a new form
   validates_numericality_of :price
   validates_numericality_of :stock, :only_integer => true, :greater_than_or_equal_to => 0
